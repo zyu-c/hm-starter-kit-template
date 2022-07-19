395f4298d526cc67213dae5b94b79d5c9b9429c4
ZyuC <m9t1k3zczc@gmail.com>
2022-07-19 23:55:22 +0900

    main関数をタイマ割り込みに対応

--- src/main.cpp
+++ src/main.cpp
@@ -3,13 +3,12 @@
 int main() {
     Mouse* mouse = Mouse::getInstance();
     mouse->init();
+    mouse->startControllerTimer();
 
     while (1) {
         mouse->ioex->set(0x00);
-        mouse->ioex->update();
         delay_ms(100);
         mouse->ioex->set(0x0F);
-        mouse->ioex->update();
         delay_ms(100);
     }
     return 0;
